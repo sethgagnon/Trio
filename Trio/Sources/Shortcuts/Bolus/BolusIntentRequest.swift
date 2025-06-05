b7438affd42ea097a0403a452d102b555e41556b
sethgagnon <597958+sethgagnon@users.noreply.github.com>
2025-06-05 11:08:13 -0400

    Allow external insulin logging regardless of bolus setting

--- Trio/Sources/Shortcuts/Bolus/BolusIntentRequest.swift
+++ Trio/Sources/Shortcuts/Bolus/BolusIntentRequest.swift
@@ -3,8 +3,25 @@ import CoreData
 import Foundation
 
 @available(iOS 16.0,*) final class BolusIntentRequest: BaseIntentsRequest {
-    func bolus(_ bolusAmount: Double) async throws -> LocalizedStringResource {
+    func bolus(_ bolusAmount: Double, externalInsulin: Bool = false) async throws -> LocalizedStringResource {
         var bolusQuantity: Decimal = 0
+
+        if externalInsulin {
+            let maxExternal = settingsManager.pumpSettings.maxBolus * 3
+            if Decimal(bolusAmount) > maxExternal {
+                return LocalizedStringResource(
+                    "The external bolus cannot be larger than \(maxExternal.description) U."
+                )
+            }
+
+            bolusQuantity = Decimal(bolusAmount)
+            await pumpHistoryStorage.storeExternalInsulinEvent(amount: bolusQuantity, timestamp: Date())
+            try await apsManager.determineBasalSync()
+            return LocalizedStringResource(
+                "Logged \(bolusQuantity.formatted()) U of external insulin."
+            )
+        }
+
         switch settingsManager.settings.bolusShortcut {
         // Block boluses if they are disabled
         case .notAllowed:
@@ -18,9 +35,8 @@ import Foundation
                 return LocalizedStringResource(
                     "The bolus cannot be larger than the pump setting max bolus (\(settingsManager.pumpSettings.maxBolus.description))."
                 )
-            } else {
-                bolusQuantity = apsManager.roundBolus(amount: Decimal(bolusAmount))
             }
+            bolusQuantity = apsManager.roundBolus(amount: Decimal(bolusAmount))
             await apsManager.enactBolus(amount: Double(bolusQuantity), isSMB: false, callback: nil)
             return LocalizedStringResource(
                 "A bolus command of \(bolusQuantity.formatted()) U of insulin was sent."
